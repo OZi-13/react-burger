75f3fc2f79be7c01a189e22890c803ed48146cc8
OZi-13 <o_zi.cdr@mail.ru>
2026-08-05 18:39:57 +0300

    FEAT: show order request state

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,14 +1,44 @@
-import { CheckMarkIcon } from '@krgaa/react-developer-burger-ui-components';
-
-import { ORDER_NUMBER } from '@utils/constants';
+import { CheckMarkIcon, Preloader } from '@krgaa/react-developer-burger-ui-components';
 
 import styles from './order-details.module.css';
 
-export const OrderDetails = (): React.JSX.Element => {
+type TOrderDetailsProps = {
+  error: string;
+  isLoading: boolean;
+  orderNumber: number | null;
+};
+
+export const OrderDetails = ({
+  error,
+  isLoading,
+  orderNumber,
+}: TOrderDetailsProps): React.JSX.Element => {
+  if (isLoading) {
+    return (
+      <article className={`${styles.details} pt-4 pb-30`}>
+        <Preloader />
+        <p className="text text_type_main-medium mt-10">Оформляем заказ</p>
+      </article>
+    );
+  }
+
+  if (error || orderNumber === null) {
+    return (
+      <article className={`${styles.details} pt-4 pb-30`}>
+        <p className="text text_type_main-medium mb-4">
+          {error || 'Не удалось получить номер заказа.'}
+        </p>
+        <p className="text text_type_main-default text_color_inactive">
+          Проверьте состав бургера и попробуйте ещё раз
+        </p>
+      </article>
+    );
+  }
+
   return (
     <article className={`${styles.details} pt-4 pb-30`}>
       <p className={`${styles.number} text text_type_digits-large mb-8`}>
-        {ORDER_NUMBER}
+        {orderNumber}
       </p>
       <p className="text text_type_main-medium mb-15">идентификатор заказа</p>
       <div className={`${styles.icon} mb-15`}>
